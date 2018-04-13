ad87e8df3f86767aed54ecb1c3896e9b2ca87d9c
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2018-04-13 17:44:16 +0530

    fetching of users, via location, language

--- hirehub/api/__init__.py
+++ hirehub/api/__init__.py
@@ -1,3 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import requests
+
+class GitAPI:
+    """Class to operate on api.github.com and provide details"""
+
+    def __init__(self):
+        """Constructs a GitAPI instance"""
+
+        self.endpoint = "https://api.github.com"
+        self.type = "application/javascript"
+
+    def search_users(self, location = 'India', language = 'any', sort_by = 'followers', order = 'desc'):
+        """Searches users using github api and gives list of users.
+        :arg location: Desired location for target developers.
+        :arg language: Specific programming language filter.
+        :arg sort_by: Sort by followers or repositories.
+        :arg order: Order by Ascending (asc) or Descending (desc)
+        """
+
+        users_endpoint = self.endpoint + "/search/users"
+
+        q = '+location:' + location
+        if(language != 'any'):
+            q = q + '+language:' + language
+
+        payload = {
+                'q'     : q,
+                'sort'  : sort_by
+                'order' : order
+            }
+
+        requests.get(users_endpoint,
+                params=payload)
+
